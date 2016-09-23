6704cb604333d7fa2d2297db70c0eb082778bac3
Sebastian Arcos Specht <sebastian.a.specht@gmail.com>
2016-09-23 11:24:10 +0200

    rodet lidt rund men det virker måske

--- src/RandomKlasse.java
+++ src/RandomKlasse.java
@@ -2,12 +2,92 @@
 public class RandomKlasse {
 
 	public static void main(String[] args) {
-
-
 		System.out.println("HEEEJ EMIR");
 		//HEY
 		//hey
+		System.out.println("Hej seba 100");
+		
+		
+		drawHeart();
+		drawBLANK();
+		drawtTOP();
+		drawtBUND();
+		drawBLANK();
+		drawtBUND();
+		drawBLANK();
+		drawL();
+		drawBLANK();
+		drawL();
+		drawBLANK();
+		drawY();
+		drawBLANK();
+		drawK();
+		drawBLANK();
+		drawK();
+		drawBLANK();
+		drawE();
+		drawBLANK();
+		drawHeart();
+
+		
 		
 	}
 
-}
+
+		
+
+
+	public static void drawtTOP() {
+		System.out.println(" ______");
+	}
+	
+	public static void drawtBUND() {
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+		System.out.println("   I   ");
+	}
+	
+	public static void drawBLANK() {
+		System.out.println("      ");
+		System.out.println("      ");
+		
+	}
+	
+	public static void drawL() {
+		System.out.println("   I   ");
+		System.out.println("   I__   ");
+		
+	}
+	
+	public static void drawY() {
+		System.out.println("  \\ /  ");
+		System.out.println("   V  ");
+		System.out.println("   I   ");
+		
+	}
+	
+	public static void drawK() {
+		System.out.println("   I/   ");
+		System.out.println("   I\\  ");
+		
+	}
+	
+	public static void drawE() {
+		System.out.println("    _   ");
+		System.out.println("   I_   ");
+		System.out.println("   I_   ");
+		
+	}
+	
+	public static void drawHeart() {
+		System.out.println("   _    _   ");
+		System.out.println(" (   \\/   ) ");
+		System.out.println("  \\      /  ");
+		System.out.println("   \\    /   ");
+		System.out.println("    \\  /    ");
+		System.out.println("     \\/     ");
+		
+	}
+	
+	
+}	
